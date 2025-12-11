ca3ff1dac28c3d6f3402c91739077579978393ea
Gavin Giordano <gavingiordano18@gmail.com>
2025-12-11 22:19:20 +0000

    add data models for user and entry

---
+++ models.py
@@ -0,0 +1,18 @@
+from datetime import datetime, timezone
+from typing import Optional
+from sqlmodel import Field, SQLModel
+
+
+class User(SQLModel, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    name: str
+    email: str = Field(unique=True)
+    password: str
+
+
+class Entry(SQLModel, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    mood_score: int = Field(ge=1, le=5)
+    comment: Optional[str] = Field(default=None)
+    created_at: datetime = Field(default_factory=lambda: datetime.now(timezone.utc))
+    user_id: int = Field(foreign_key="user.id")
