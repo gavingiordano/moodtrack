24e5bfe1253abfe9316def39c87a2b88127c91e7
Gavin Giordano <gavingiordano18@gmail.com>
2025-12-11 23:40:19 +0000

    connect fastapi to database

---
+++ main.py
@@ -0,0 +1,12 @@
+from fastapi import FastAPI
+from database import create_db_and_tables
+from contextlib import asynccontextmanager
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    create_db_and_tables()
+    yield
+
+
+app = FastAPI(lifespan=lifespan)
