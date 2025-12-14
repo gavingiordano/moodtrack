24c30d2df89cb32302d4f91010463051cb8a6d27
Gavin Giordano <gavingiordano18@gmail.com>
2025-12-14 02:05:59 +0000

    add get_current_user function

--- app/auth.py
+++ app/auth.py
@@ -2,9 +2,11 @@ import os
 from datetime import datetime, timedelta, timezone
 from typing import Optional
 
+from database import get_session
 from dotenv import load_dotenv
-from fastapi import Depends
-from jose import jwt
+from fastapi import Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer
+from jose import JWTError, jwt
 from models import User
 from passlib.context import CryptContext
 from sqlmodel import Session, select
@@ -16,6 +18,7 @@ ALGORITHM = os.environ["ALGORITHM"]
 
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
 
 def verify_password(plain_password: str, hashed_password: str) -> bool:
@@ -44,3 +47,22 @@ def create_access_token(data: dict[str, str | datetime], expires_delta: Optional
     to_encode.update({"exp": expire})
     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
     return encoded_jwt
+
+
+async def get_current_user(token: str = Depends(oauth2_scheme), session: Session = Depends(get_session)) -> User:
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer", }
+    )
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        username: str = payload.get("sub")
+        if username is None:
+            raise credentials_exception
+    except JWTError:
+        raise credentials_exception
+    user = session.exec(select(User).where(User.username == username)).first()
+    if user is None:
+        raise credentials_exception
+    return user
